640193a8a6eccd43d98c7ee1b2f44a693941b7cd
dougshidong <doug.shidong@gmail.com>
2016-12-02 16:25:59 -0500

    Done project 3

--- project3/euler1d.f90
+++ project3/euler1d.f90
@@ -7,34 +7,95 @@ program euler1d
     use flux
 
     implicit none
-    real(p2) :: a = -50.0d0, b = 50.0d0, dx, diaphragm = 0.0d0
-    real(p2) :: CFL, t, tf = 25.0d0, max_uc, dt
+    real(p2) :: a = 0.0d0, b = 1.50d0, dx, diaphragm = 0.0d0
+    real(p2) :: CFL, t, tf = 0.01d0, max_uc, dt, ta, tb
     real(p2), allocatable :: x(:), U(:,:), W(:,:), c(:)
-    real(p2) :: WL_ini(3), WR_ini(3) ! Initial Left and Right values
-    integer  :: ix, iw
+    real(p2) :: W1(3), W2(3) ! Initial Left and Right values
+    integer  :: ix, iw, itime, nt
     character(len=40) :: outn
     outn = 'sol.dat'
 
-    CFL = 1.0
-    nw = 100
+    CFL = 0.5
+    nw = 150
     nhalo = 2
     ntot = nw + 2*nhalo
-    flux_scheme = 1
-    ! Initialize variables give test case ID
-    WL_ini(1) = 1.0d0
-    WL_ini(2) = 0.0d0
-    WL_ini(3) = 2.0d0
-    WR_ini(1) = 1.0d0
-    WR_ini(2) = 0.0d0
-    WR_ini(3) = 1.0d0
+    ! Initial conditions
+    W1(1) = 1.0d0
+    W1(2) = 0.0d0
+    W1(3) = 1.0d0
+    W2(1) = 100.0d0
+    W2(2) = 0.0d0
+    W2(3) = 1000.0d0
     flux_scheme = 4
-    do ilim = 1, 5
+    ilim = 5
+
+    ix = 2
+    select case(ix) ! Output solutions at different time intervals
+        case(1)
+            ta =-0.01d0
+            tb = 0.15d0
+        case(2)
+            ta = 0.14d0
+            tb = 0.30d0
+        case(3)
+            ta = 0.29d0
+            tb = 0.45d0
+    end select
+    nt = 16 ! # of time intervals
+    do itime = 1, nt
+        tf = ta + (tb - ta) / nt * itime
         call initialize
+        write(outn,'(A4,I1,A4)') 'time',itime,'.dat'
+        if(itime.ge.10) write(outn,'(A4,I2,A4)') 'time',itime,'.dat'
         t = 0.0d0
         do while(t.lt.tf)
+            if(t.eq.0.0d0) ilim = 1
+            if(t.ne.0.0d0) ilim = 4
             max_uc = maxval(abs(W(2,:) + c(:)))
             dt = CFL * dx / max_uc
-            dt = 0.5
+            if((t + dt).gt.tf) then
+                dt = tf - t
+                t  = tf
+            else
+                t = t + dt
+            end if
+            call eval_flux(U, W, dt, dx)
+            !write(*,*) 'Time ', t, ' of', tf, fluxes(3,42)
+            do ix = 1, nw
+                iw = ix + nhalo
+                U(:,iw) = U(:,iw) - dt / dx * (fluxes(:,iw+1) - fluxes(:,iw))
+            end do
+            call update_W_c(U, W, c, gam)
+            call update_wall(W)
+        end do
+        open(unit=40,file=outn,form='FORMATTED')
+        do ix = 1, nw
+            iw = ix + nhalo
+            write(40,'(5(E23.14))') x(iw), W(1,iw), W(2,iw), W(3,iw), tf
+        end do
+        close(40)
+        !call output
+        deallocate(x, U, W, c, fluxes, slopes)
+    end do
+
+    ! Pressure wrt to time computation for nw = 150, 300, 600, 1200, 2400, 4800
+    nt = 6
+    nw = 75
+    CFL = 0.5
+    do itime = 1, nt
+        nw = nw * 2
+        ntot = nw + 2*nhalo
+        tf = 0.45d0
+        call initialize
+        write(outn,'(A4,I1,A4)') 'size',itime,'.dat'
+        if(itime.ge.10) write(outn,'(A4,I2,A4)') 'size',itime,'.dat'
+        open(unit=40,file=outn,form='FORMATTED')
+        t = 0.0d0
+        do while(t.lt.tf)
+            if(t.eq.0.0d0) ilim = 1
+            if(t.ne.0.0d0) ilim = 4
+            max_uc = maxval(abs(W(2,:) + c(:)))
+            dt = CFL * dx / max_uc
             if((t + dt).gt.tf) then
                 dt = tf - t
                 t  = tf
@@ -49,8 +110,15 @@ program euler1d
             end do
             call update_W_c(U, W, c, gam)
             call update_wall(W)
+            do ix = 1, ntot
+              if(x(ix)-1.30d0.ge.0.0d0) then
+                write(*,'(3(E23.14))') t, W(3,ix), x(ix)
+                write(40,'(3(E23.14))') t, W(3,ix), x(ix)
+                exit
+              end if
+            end do
         end do
-        call output
+        close(40)
         deallocate(x, U, W, c, fluxes, slopes)
     end do
 
@@ -80,10 +148,10 @@ program euler1d
             ! Calculated at the middle point of each segment
             do i = 1, nw + 2*nhalo
                 x(i) = a + dx * (i-nhalo - 1) + dx / 2.0d0
-                if(x(i).le.diaphragm) then
-                    W(:,i) = WL_ini
+                if(x(i).ge.-0.1d0 .and. x(i).le.0.1d0) then
+                    W(:,i) = W2
                 else
-                    W(:,i) = WR_ini
+                    W(:,i) = W1
                 end if
             end do
             call update_U_c(U, W, c, gam)
@@ -99,8 +167,12 @@ program euler1d
         subroutine output
         implicit none
         open(unit=40,file=outn,form='FORMATTED')
-        do ix = 1, nw
-            iw = ix + nhalo
+        !do ix = 1, nw
+        !    iw = ix + nhalo
+        !    write(40,'(4(E23.14))') x(iw), W(1,iw), W(2,iw), W(3,iw)
+        !end do
+        do ix = 1, ntot
+            iw = ix
             write(40,'(4(E23.14))') x(iw), W(1,iw), W(2,iw), W(3,iw)
         end do
         close(40)
---
+++ project3/plot.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
+fname = ['ZERO','AVG','MINMOD']
+fname = ['MINMOD','SUPERBEE', 'VANLEERNS']
+col   = ['b','r','g','c']
+symb  = ['o','s','v','^']
+
+inname = 'exact.dat'
+xe, re, ue, pe = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+inname = 'exact100.dat'
+xe100, re100, ue100, pe100 = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+
+pp = PdfPages('task12.pdf')
+fig, axarr = plt.subplots(3, sharex=True,figsize=(8, 8))
+for caseid in range(3):
+    inname = fname[caseid] + '.dat'
+    outname = fname[caseid] + '.pdf'
+
+    x, r, u, p = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+
+#   axarr[0].set_title(fname[caseid])
+
+    axarr[0].plot(xe, re, '-k')
+    axarr[0].plot(x, r, symb[caseid],mec=col[caseid],mfc='none',ms=3, label=fname[caseid])
+    axarr[0].set_ylabel(r'\rho')
+
+    axarr[1].plot(xe, ue, '-k')
+    axarr[1].plot(x, u, symb[caseid],mec=col[caseid],mfc='none',ms=3)
+    axarr[1].set_ylabel(r'u')
+
+    axarr[2].plot(xe, pe, '-k')
+    axarr[2].plot(x, p, symb[caseid],mec=col[caseid],mfc='none',ms=3)
+    axarr[2].set_ylabel(r'p')
+    axarr[2].set_xlabel(r'x')
+
+    axarr[0].set_ylim([0.75, 1.6])
+    axarr[1].set_ylim([-0.02, 0.40])
+    axarr[2].set_ylim([0.9, 2.1])
+    axarr[0].set_xlim([-50, 50])
+    axarr[1].set_xlim([-50, 50])
+    axarr[2].set_xlim([-50, 50])
+
+    print 'error'
+    print np.linalg.norm( (re100-r) ,ord=2)/ (104**(0.5))
+    print np.linalg.norm( (ue100-u) ,ord=2)/ (104**(0.5))
+    print np.linalg.norm( (pe100-p) ,ord=2)/ (104**(0.5))
+
+#for i in range(3):
+#    axarr[i].axvline(x= 8.655,color='k',ls='-', label='Contact Surface')
+#    axarr[i].axvline(x= -41.75,color='k',ls='--',label='Left Rarefaction Head')
+#    axarr[i].axvline(x= -31.35,color='k',ls='-.',label='Left Rarefaction Tail')
+#    axarr[i].axvline(x= 35.35,color='k',ls=':', label='Right Shockwave')
+axarr[0].legend(loc=2, fontsize = 'xx-small')
+
+#axarr[0].plot(xe100, re100, '-^c')
+#axarr[0].set_ylabel(r'\rho')
+#
+#axarr[1].plot(xe100, ue100, '-^c')
+#axarr[1].set_ylabel(r'u')
+#
+#axarr[2].plot(xe100, pe100, '-^c')
+#axarr[2].set_ylabel(r'p')
+#axarr[2].set_xlabel(r'x')
+
+plt.tight_layout()
+pp.savefig(bbx_inches = 'tight')
+pp.close()
+
+######################
+
+nt = 16
+cmap = plt.get_cmap('jet')
+colors = cmap(np.linspace(0, 1.0, nt))
+
+fname = ['sol']
+pp = PdfPages('task21.pdf')
+fig, axarr = plt.subplots(3, sharex=True,figsize=(8, 8))
+fname = ['']*nt
+print fname
+for i in range(nt):
+    fname[i] = 'time%d'%(i+1)
+    
+print fname
+for caseid in range(nt):
+    inname = fname[caseid] + '.dat'
+
+    x, r, u, p, tf = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+
+#   axarr[0].set_title(fname[caseid])
+
+    axarr[0].plot(x, r, '-',color=colors[caseid],mfc='none',ms=3, label='t = %f'%tf[0])
+    axarr[0].set_ylabel(r'\rho')
+
+    axarr[1].plot(x, u, '-',color=colors[caseid],mfc='none',ms=3)
+    axarr[1].set_ylabel(r'u')
+
+    axarr[2].plot(x, p, '-',color=colors[caseid],mfc='none',ms=3)
+    axarr[2].set_ylabel(r'p')
+    axarr[2].set_xlabel(r'x')
+
+#   axarr[0].set_ylim([0, 30])
+#   axarr[1].set_ylim([-10, 10])
+#   axarr[2].set_ylim([0, 400])
+    axarr[0].set_xlim([0, 1.6])
+    axarr[1].set_xlim([0, 1.6])
+    axarr[2].set_xlim([0, 1.6])
+
+axarr[0].legend(loc=2, ncol=2, fontsize = 'xx-small')
+
+plt.tight_layout()
+pp.savefig(bbx_inches = 'tight')
+pp.close()
+
+######################
+nt = 6
+
+pp = PdfPages('task22.pdf')
+fig = plt.figure(figsize=(8, 4))
+fname = ['']*nt
+print fname
+for i in range(nt):
+    fname[i] = 'size%d'%(i+1)
+    
+print fname
+for caseid in range(nt):
+    inname = fname[caseid] + '.dat'
+
+    t, p, x = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+
+#   axarr[0].set_title(fname[caseid])
+
+    plt.plot(t, p, '-',mfc='none',ms=3, label='size = %d'%caseid)
+    plt.ylabel(r'p')
+    plt.xlabel(r't')
+
+
+plt.legend(loc=2, ncol=2, fontsize = 'xx-small')
+
+plt.tight_layout()
+pp.savefig(bbx_inches = 'tight')
+pp.close()
+
+pp = PdfPages('task22_peak.pdf')
+fig = plt.figure(figsize=(8, 4))
+fname = ['']*nt
+print fname
+for i in range(nt):
+    fname[i] = 'size%d'%(i+1)
+    
+gridsize = np.empty(nt,dtype=np.float64)
+maxp = np.empty(nt,dtype=np.float64)
+
+nx = 75
+for caseid in range(nt):
+    inname = fname[caseid] + '.dat'
+
+    t, p, x = np.loadtxt(inname, dtype = np.float64, skiprows=1, unpack=True)
+    nx = nx*2
+    dx = 1.5/nx
+    gridsize[caseid] = nx
+    maxp[caseid] = max(p)
+print gridsize
+plt.plot(gridsize, maxp, '-o',mfc='none',ms=3, label='size = %d'%caseid)
+plt.xlabel(r'nx')
+plt.ylabel(r'max($p(x=1.3,t)$)')
+
+plt.tight_layout()
+pp.savefig(bbx_inches = 'tight')
+pp.close()
